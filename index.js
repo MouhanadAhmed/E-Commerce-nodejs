e21209d8048115161008fc8d467267db32a9b1ae
Mouhanad Ahmed <mouhanad.aboelmakarem@gmail.com>
2023-09-23 04:42:49 +0300

    cors

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ import { dbConnection } from './Database/dbConnection.js';
 import { server } from './src/server.js';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
-
+import cors from 'cors';
 
 dotenv.config();
 
 
 const app = express()
 const port = 3000
+app.use(cors())
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(express.static('uploads'))
 server(app)
 dbConnection();
-app.listen(port, () => console.log(`E-commerce app listening on port ${port}!`))
+app.listen(process.env.PORT || port, () => console.log(`E-commerce app listening on port ${port}!`))
 
 process.on('unhandledRejection', (err)=> console.log(err))
