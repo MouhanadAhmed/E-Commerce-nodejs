f52b0ba952f110c69af4ed5ab3aab581e9bb8612
Mouhanad Ahmed <mouhanad.aboelmakarem@gmail.com>
2023-09-23 04:22:36 +0300

    atlas

--- Database/dbConnection.js
+++ Database/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 
 export function dbConnection() {
-    mongoose.connect('mongodb://127.0.0.1:27017/E-commerce').then(()=>{
+    mongoose.connect('mongodb+srv://mouhanadahmed2:va44ws167MraIjD6@mymongodb.l1ihnxo.mongodb.net/E-commerce').then(()=>{
         console.log("db connected");
     }).catch((err)=>{
         console.log(`db Error ${err}`);
